ba3fbb57f2e5f89633ee072f8080629fc4f1091b
Jeong Lim <jelim@starbucks.com>
2017-10-15 13:34:27 -0700

    Save WIP

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import FetchButton from './components/button/Fetch/Fetch.Component';
+import './App.css';
+
+export default function ()  {
+  return <FetchButton />;
+}
---
+++ src/components/button/Fetch/Fetch.Component.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Button } from './Style';
+
+const FetchButton = () => (
+  <Button />
+);
+
+export default FetchButton;
